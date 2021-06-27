e8c32d9c796dec455f486960b62b85f20c9d198e
Shawn Eddy <ymiseddy@gmail.com>
2021-06-27 13:42:29 -0400

    Converted to clip - commands all work.
    This still requires some work on the docs.

--- vault.py
+++ vault.py
@@ -1,25 +1,19 @@
+import os
+import pathlib
+import sys
+import time
+from select import select
+
 import click
 import gnupg
-import os
-import time
-import yaml
-import sys
-import pathlib
-import string
-import secrets
-import subprocess
 import pyotp
 import pyperclip
-
-from select import select
-from pathlib import Path
-from itertools import islice
+import yaml
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.validation import Validator, ValidationError
 
 # Debug only
-from icecream import ic
 
 # Need a MSVcrt for windows
 if os.name == "nt":
@@ -86,7 +80,6 @@ def try_read_config():
         sys.exit(-1)
 
 
-
 def decrypt(name):
     infile = check_file(name) + ".gpg"
 
@@ -120,7 +113,8 @@ class ExistsInList(Validator):
 
     def validate(self, document):
         if not document.text in self.items:
-            raise ValidationError(message = f"Secret {document.text} does not exist.")
+            raise ValidationError(message=f"Secret {document.text} does not exist.")
+
 
 def prompt_name(must_exist=False):
     include_directories = not must_exist
@@ -128,7 +122,7 @@ def prompt_name(must_exist=False):
     if must_exist and not secrets:
         print("There are no secrets to list.  Try adding some.")
         sys.exit(-1)
-    secret_completer  = WordCompleter(secrets, sentence=True, match_middle=True)
+    secret_completer = WordCompleter(secrets, sentence=True, match_middle=True)
     validator = None
     if must_exist:
         validator = ExistsInList(secrets)
@@ -149,7 +143,7 @@ def check_file(path):
 
 def prompt_key():
     gpg_keys = get_key_list()
-    secret_completer  = WordCompleter(gpg_keys, sentence=True, match_middle=True)
+    secret_completer = WordCompleter(gpg_keys, sentence=True, match_middle=True)
     validator = ExistsInList(gpg_keys)
 
     name = prompt("GPG Key: ", validator=validator, completer=secret_completer)
@@ -166,9 +160,26 @@ def totp(url: str):
     return otp.now()
 
 
+def write_encrypted(name, data):
+    config = read_config()
+    outfile = check_file(name) + ".gpg"
+
+    gpg = gnupg.GPG()
+    key = config["keyid"]
+    data = gpg.encrypt(data.encode("utf-8"), key)
+
+    data_path = os.path.dirname(outfile)
+    if not os.path.exists(data_path):
+        pathlib.Path(data_path).mkdir(parents=True)
+
+    with open(outfile, "w") as of:
+        of.write(str(data))
+
+
 class AliasedGroup(click.Group):
 
     def get_command(self, ctx, cmd_name):
+        print(f"resolving {cmd_name}")
         rv = click.Group.get_command(self, ctx, cmd_name)
         if rv is not None:
             return rv
@@ -178,19 +189,21 @@ class AliasedGroup(click.Group):
 
         return click.Group.get_command(self, ctx, AliasedGroup.aliases[cmd_name])
 
-
     def resolve_command(self, ctx, args):
         # always return the full command name
         _, cmd, args = super().resolve_command(ctx, args)
         return cmd.name, cmd, args
+
+
 AliasedGroup.aliases = {}
 
 
-def alias(*args):
+def alias(name, alt=None):
     def do_handle(cb):
-        for name in args:
-            actual_command = cb.__name__
-            AliasedGroup.aliases[name] = actual_command
+        command = alt
+        if alt is None:
+            command = cb.__name__
+        AliasedGroup.aliases[name] = command
         return cb
 
     return do_handle
@@ -199,6 +212,7 @@ def alias(*args):
 @click.group(cls=AliasedGroup)
 def cli():
     pass
+
 
 @cli.command()
 @alias("i")
@@ -234,13 +248,25 @@ def init(keyname=None):
     print("Vault initialized.")
 
 
-
 @cli.command()
 @alias("a")
 @click.argument("name", required=False)
 def add(name=None):
     """ Add a secret to the vault. """
-    config = try_read_config()
+    try_read_config()
+    if name is None:
+        name = prompt_name()
+
+    outfile = check_file(name) + ".gpg"
+
+    pass1 = prompt("Password:", is_password=True)
+    pass2 = prompt("Verify:", is_password=True)
+
+    if pass1 != pass2:
+        print("Passwords do not match.")
+        sys.exit(-1)
+
+    write_encrypted(name, pass1)
 
 
 @cli.command()
@@ -248,13 +274,102 @@ def add(name=None):
 @click.argument("name", required=False)
 def edit(name=None):
     """ create or edit a new multi-line secret. """
-    config = try_read_config()
+
+    try_read_config()
+    if name is None:
+        name = prompt_name()
+    dest_file = check_file(name) + ".gpg"
+
+    default = ""
+    if os.path.exists(dest_file):
+        default = str(decrypt(name))
+
+    print("Multiline editor - press M-Enter (or escapt then enter) when done.")
+    edited = prompt(default=default, multiline=True)
+
+    write_encrypted(name, edited)
+
+
+@cli.command()
+@click.argument("name", required=False)
+@alias("s")
+def show(name=None):
+    try_read_config()
+    if name is None:
+        name = prompt_name(True)
+
+    decrypted = str(decrypt(name))
+
+    # If it's an OTP url, return the token instead
+    # of the data.
+    if is_otp_url(decrypted):
+        decrypted = totp(decrypted)
+
+    print(decrypted)
+
+
+@cli.command(name="list")
+@alias("ls", alt="list")
+def do_list():
+    try_read_config()
+    secret_list = get_secret_names()
+    for s in secret_list:
+        print(s)
+
+
+@cli.command(name="import")
+@alias("i")
+@click.argument("name", required=True)
+def do_import(name=None):
+    try_read_config()
+
+    if name is None:
+        name = prompt_name()
+
+    data = sys.stdin.read()
+    write_encrypted(name, data)
+
+
+@cli.command(name="otp")
+def import_otp():
+    try_read_config()
+
+    for data in sys.stdin.readlines():
+        data = data.strip()
+        otp = pyotp.parse_uri(data)
+        if otp.issuer is not None:
+            name = os.path.join("otp", otp.issuer, otp.name)
+        else:
+            name = os.path.join("otp", otp.name)
+        write_encrypted(name, data)
+
+
+@cli.command()
+@click.argument("name", required=False)
+def clip(name=None):
+    try_read_config()
+    if name is None:
+        name = prompt_name(True)
+    decrypted = str(decrypt(name))
+
+    # If it's an OTP url, return the token instead
+    # of the data.
+    if is_otp_url(decrypted):
+        decrypted = totp(decrypted)
+
+    pyperclip.copy(decrypted)
+    print("Password copied to clipboard - this will clear in 20 seconds or if you press a key.")
+    timeout = 20
+    kb_timout_hit(timeout)
+    pyperclip.copy("")
+
 
 @cli.command()
 @click.argument("name", required=False)
 def remove(name=None):
     """ remove a secret from the vault. """
     config = try_read_config()
+
 
 if __name__ == "__main__":
     read_config()
